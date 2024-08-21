bf83a17bb4da80666905c79afe29b3ce9b767a9f
Yash Singh <yashsinghofficial04@gmail.com>
2024-08-21 19:40:23 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,8 +6,13 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
+
+const __dirname = path.resolve();
 
 const app= express()
+
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -27,6 +32,12 @@ app.listen(PORT, () => console.log(`Server is Started On ${PORT}`));
 app.use('/api/user' , userRouter);
 app.use('/api/auth' , authRouter)
 app.use('/api/listing' , listingRouter)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
 
 // Middleware
 app.use((err, req, res, next) => {
